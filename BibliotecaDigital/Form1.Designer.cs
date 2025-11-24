7d80679bd0369f61bf89c9dcfedf885f2d855a91
BettleJuice <asdiazm@uamv.edu.ni>
2025-11-24 02:17:57 -0600

    Cree el diseño del formulario principal del programa para la biblioteca Digital, decidi no crear una ventana previa al programa por el tiempo que tengo.

--- BibliotecaDigital/Form1.Designer.cs
+++ BibliotecaDigital/Form1.Designer.cs
@@ -28,13 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvLibros = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbBuscar = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnTodos = new System.Windows.Forms.Button();
+            this.btnAnalisis = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLibros)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvLibros
+            // 
+            this.dgvLibros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLibros.Location = new System.Drawing.Point(82, 112);
+            this.dgvLibros.Name = "dgvLibros";
+            this.dgvLibros.RowHeadersWidth = 51;
+            this.dgvLibros.RowTemplate.Height = 24;
+            this.dgvLibros.Size = new System.Drawing.Size(655, 282);
+            this.dgvLibros.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Buscar libro:";
+            // 
+            // tbBuscar
+            // 
+            this.tbBuscar.Location = new System.Drawing.Point(166, 49);
+            this.tbBuscar.Name = "tbBuscar";
+            this.tbBuscar.Size = new System.Drawing.Size(160, 22);
+            this.tbBuscar.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Titulo",
+            "Autor",
+            "Descripción"});
+            this.comboBox1.Location = new System.Drawing.Point(332, 49);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(459, 47);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(129, 26);
+            this.btnBuscar.TabIndex = 4;
+            this.btnBuscar.Text = "BUSCAR";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // btnTodos
+            // 
+            this.btnTodos.Location = new System.Drawing.Point(459, 79);
+            this.btnTodos.Name = "btnTodos";
+            this.btnTodos.Size = new System.Drawing.Size(129, 27);
+            this.btnTodos.TabIndex = 5;
+            this.btnTodos.Text = "VER TODOS";
+            this.btnTodos.UseVisualStyleBackColor = true;
+            // 
+            // btnAnalisis
+            // 
+            this.btnAnalisis.Location = new System.Drawing.Point(594, 79);
+            this.btnAnalisis.Name = "btnAnalisis";
+            this.btnAnalisis.Size = new System.Drawing.Size(143, 26);
+            this.btnAnalisis.TabIndex = 6;
+            this.btnAnalisis.Text = "VER MÁS ANTIGUO";
+            this.btnAnalisis.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAnalisis);
+            this.Controls.Add(this.btnTodos);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.tbBuscar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvLibros);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLibros)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvLibros;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbBuscar;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnTodos;
+        private System.Windows.Forms.Button btnAnalisis;
     }
 }
 
---
+++ BibliotecaDigital/Modelos/Libro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaDigital.Modelos
+{
+    public class Libro
+    {
+        public string Codigo { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int Año { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
